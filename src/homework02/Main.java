e88a76fc19fa4fdc76015b522189e3f34aeaf87d
Aleksey939 <Aleksey93919@gmail.com>
2018-09-02 21:12:24 +0300

    Initial commit

---
+++ src/homework02/Main.java
@@ -0,0 +1,89 @@
+package homework02;
+
+
+class  ListException extends Exception{
+
+
+}
+
+class  EmptyListException  extends ListException{
+
+
+}
+
+class FullListException extends ListException {
+
+
+}
+
+class List {
+    Object[] obj;
+    int size = -1;
+    int cur = -1;
+
+    List(int size) {
+        cur = 0;
+        this.size = size;
+        obj = new Object[size];
+    }
+
+    void add(Object item) throws FullListException {
+        obj[cur++] = item;
+        if (cur >= size)
+        {
+            throw new FullListException();
+        }
+
+
+    }
+
+    void removeLast() throws EmptyListException{
+        obj[cur--] = null;
+        if (cur < size)
+        {
+            throw new EmptyListException();
+        }
+
+
+    }
+
+    int size() {
+        return obj.length;
+    }
+ }
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new List(1);
+        try {
+            list.add(265);
+            list.add(265);
+            list.add(265);
+        } catch (ListException ex) {
+            System.out.println( "Ошибка  "+ ex.toString());
+
+        }
+
+//        try {
+//            list.removeLast();
+//            list.removeLast();
+//            list.removeLast();
+//        } catch (ListException ex) {
+//            System.out.println( "Ошибка  "+ ex.toString());
+//
+//        }
+
+        Integer size = list.size();
+        System.out.println("Размер List " + size);
+    }
+}
+
+
+
+
+
+
+
+
